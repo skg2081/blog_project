14d8bfde87572e161f6bc309f72119ce6e028575
saurabh <skg2081@gmail.com>
2021-08-28 16:09:50 +0530

    comitting the model branch

--- puppycompanyblog/__init__.py
+++ puppycompanyblog/__init__.py
@@ -1,7 +1,30 @@
 # puppycompanyblog/__init__.py
+import os
 from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy 
+from flask_migrate import Migrate 
+from flask_login import LoginManager 
 
 app=Flask(__name__)
+
+##################
+# Database setup 
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+app.config['SQLALCHEMY_TRACK_MODIFICATION'] = False 
+
+db = SQLAlchemy(app) 
+Migrate(app.db) 
+
+######################
+# login config setup 
+
+login_manager  = LoginManager() 
+login_manager.init_app(app) 
+login_manager.login_view = 'users.login'
+
+#######################
 
 from puppycompanyblog.core.views import core 
 from puppycompanyblog.error_pages.handlers import error_pages
